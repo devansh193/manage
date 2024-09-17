ecba33be36cea29db4c0bc607a4a5fb1a3b7d370
devansh <devanshverma193@gmail.com>
2024-09-17 23:26:51 +0530

    init

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hi there
+      <div className="flex items-center justify-center">
+        <h1>
+          hi there
+        </h1>
+      </div>
     </main>
   );
 }
